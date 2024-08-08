dff3358f418d0e44157e08f00384969804ac3ffa
JevSantos <jeverson.apsantos@gmail.com>
2024-08-07 21:40:01 -0300

    Estrutura de classes 2 e 3

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,5 +1,14 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.print ("Olá turma! Sejam bem vindos");
+
+        String primeiroNome = "Jeverson";
+        String segundoNome = "Santos";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.err.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método = " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
